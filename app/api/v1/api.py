273eb9c24d20f48397cb1fe3a527ac5c801ac1c2
manhnv <manh117bg@gmail.com>
2021-12-12 10:11:21 +0700

    update test

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from .endpoints import login
+from .endpoints import login, words
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["User-management"], prefix="/users")
+api_router.include_router(words.router, tags=["Word-management"], prefix="/words")
